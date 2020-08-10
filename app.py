189b621a0649dd9230cc5f5d7112c63ff66f50b5
Lapa <thinklnp@yandex.ru>
2020-08-10 23:18:18 +0300

    simple service

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/api/')
+def api_work():
+    try:
+        n = int(request.args.get('n'))
+        if n > 0:
+            return jsonify(m=n + 1, some_data=''.join([str(n) for x in range(n)]))
+    except:
+        return render_template('hello.html')
