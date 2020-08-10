b6d37504635f8ca7c7a3e4df4f9d0b242a9cee1f
Lapa <thinklnp@yandex.ru>
2020-08-10 23:39:22 +0300

    add db interfacing

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
+
+with sqlite3.connect('inner.db') as cn:
+    crs = cn.cursor()
+    crs.execute('DROP TABLE IF EXISTS t;')
+    cn.commit()
+    crs.execute('CREATE TABLE t(key TEXT PRIMARY KEY, q INT)')
+    cn.commit()
 
 
 @app.route('/api/')
 def api_work():
     try:
-        n = int(request.args.get('n'))
-        if n > 0:
-            return jsonify(m=n + 1, some_data=''.join([str(n) for x in range(n)]))
-    except:
+        key = request.args.get('key')
+        q = int(request.args.get('q'))
+        with sqlite3.connect('inner.db') as cn:
+            crs = cn.cursor()
+            crs.execute('''
+            INSERT INTO t(key, q) VALUES (:key, :q) 
+            ON CONFLICT(key) DO UPDATE
+            SET q = q + :q
+            WHERE key = :key
+            ''', {'key':key, 'q':q})
+            cn.commit()
+            crs.execute('SELECT key, q FROM t WHERE key = ?', key)
+            rs = crs.fetchall()
+        if rs:
+            return jsonify(key=rs[0][0], q=rs[0][1])
+    except Exception as e:
+        app.logger.error(e)
         return render_template('hello.html')
