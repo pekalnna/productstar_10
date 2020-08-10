4be323ad42b20a335c80fe80bb6f5ecad4b6defb
Lapa <thinklnp@yandex.ru>
2020-08-10 22:34:30 +0300

    add more paths

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/first')
+def hello_first():
+    return 'Hello, First!'
+
+
+@app.route('/second')
+def hello_second():
+    return 'Hello, Next!'
